b0503fe30987e8252a6ca5a8ef37ad2b6f1ad6c5
Korkin Alexsandr <korkin.06@inbox.ru>
2025-05-22 16:55:42 +0300

    Added DbContext

---
+++ HostelProperty.DataAccess/MyDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HostelProperty.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using HostelProperty.DataAccess.Configurations;
+
+namespace HostelProperty.DataAccess
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Resident> Residents { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomSubject> RoomSubjects { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ResidedntConfiguration());
+            modelBuilder.ApplyConfiguration(new RoomConfiguration());
+            modelBuilder.ApplyConfiguration(new RoomSubjectConfiguration());
+            modelBuilder.ApplyConfiguration(new SubjectConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
